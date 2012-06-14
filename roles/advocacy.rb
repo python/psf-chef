aa2ace465222a478bb035e6d3d6b84e316d70cde
Noah Kantrowitz <noah@coderanger.net>
2012-06-14 13:50:33 -0700

    Stubs for advocacy site.

---
+++ roles/advocacy.rb
@@ -0,0 +1,5 @@
+name "advocacy"
+description "PSF advocacy site"
+run_list [
+  "recipe[psf-advocacy]",
+]
---
+++ site-cookbooks/psf-advocacy/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Martin von Löwis"
+maintainer_email ""
+license          "Apache 2.0"
+description      "Configuration related to the PSF advocacy site"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
