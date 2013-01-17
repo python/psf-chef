c4198afca1cffdbf0114c0b7d935ceb12fd448d2
Noah Kantrowitz <noah@coderanger.net>
2013-01-17 01:04:46 -0800

    Have the rsnapshot server backup all postgres DBs.

--- roles/rsnapshot.rb
+++ roles/rsnapshot.rb
@@ -2,4 +2,4 @@ name "rsnapshot"
 description "RSnapshot backup server"
 # Owner: Noah Kantrowitz <noah@coderanger.net>
 
-run_list 'recipe[rsnapshot::server]'
+run_list 'recipe[rsnapshot::server]', 'recipe[psf-rsnapshot::postgres]'
