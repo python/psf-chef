8c44c7feff02a06c4d4c17b002def1beebfd0b58
Noah Kantrowitz <noah@coderanger.net>
2013-01-22 00:38:55 -0800

    Turn down how many backups we keep due to running out of space.

--- roles/rsnapshot.rb
+++ roles/rsnapshot.rb
@@ -3,3 +3,19 @@ description "RSnapshot backup server"
 # Owner: Noah Kantrowitz <noah@coderanger.net>
 
 run_list 'recipe[rsnapshot::server]', 'recipe[psf-rsnapshot::postgres]'
+
+override_attributes({
+  rsnapshot: {
+    server: {
+      retain: {
+        hourly: {
+          count: 4,
+          hour: '*/6',
+        },
+        daily: {
+          count: 7,
+        }
+      },
+    },
+  },
+})
