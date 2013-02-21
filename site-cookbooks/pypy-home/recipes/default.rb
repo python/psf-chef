727089a5f22ca4b41832d474130f75c9efd577d2
Noah Kantrowitz <noah@coderanger.net>
2013-02-20 22:18:19 -0800

    Whipping pypy-home into working.

--- site-cookbooks/pypy-home/recipes/default.rb
+++ site-cookbooks/pypy-home/recipes/default.rb
@@ -1,9 +1,13 @@
+include_recipe 'mercurial'
+
 application "pypy.org" do
   path "/srv/pypy.org"
   repository "https://bitbucket.org/pypy/pypy.org"
+  revision 'tip'
   scm_provider Chef::Provider::Mercurial
 
   nginx_load_balancer do
-    static_files "/" => ""
+    template "nginx.conf.erb"
+    static_files "/" => "/srv/pypy.org/current"
   end
 end
