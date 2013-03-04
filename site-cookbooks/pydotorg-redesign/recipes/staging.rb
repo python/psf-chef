8f90dc3365ef7436ba3b47126a2b50f1fc82e745
Jacob Kaplan-Moss <jacob@jacobian.org>
2013-03-04 17:04:26 -0600

    Work around http://tickets.opscode.com/browse/CHEF-2784.

--- site-cookbooks/pydotorg-redesign/recipes/staging.rb
+++ site-cookbooks/pydotorg-redesign/recipes/staging.rb
@@ -9,29 +9,45 @@ include_recipe 'python'
 #
 
 class ::Chef
-  class Resource
-    class ApplicationPydotorgDjango < ApplicationPythonDjango
-      attribute :python_interpreter, :default => '/usr/bin/python'
-    end
-  end
-  class Provider
-    class ApplicationPydotorgDjango < ApplicationPythonDjango
-      def install_packages
-        python_virtualenv new_resource.virtualenv do
-          path new_resource.virtualenv
-          interpreter new_resource.python_interpreter
-          action :create
+    class Resource
+        # Override the resource to add a "python_interpreter" option.
+        class ApplicationPydotorgDjango < ApplicationPythonDjango
+            attribute :python_interpreter, :default => '/usr/bin/python'
         end
-        new_resource.packages.each do |name, ver|
-          python_pip name do
-            version ver if ver && ver.length > 0
-            virtualenv new_resource.virtualenv
-            action :install
-          end
-        end
-      end
     end
-  end
+    class Provider
+        class ApplicationPydotorgDjango < ApplicationPythonDjango
+            # Now override install_packages to pick up the attribute above. I
+            # didn't copypasta the bit that installs packages from a  packages
+            # attribute since I'm not using that here, it's all in requirements.
+            def install_packages
+                python_virtualenv new_resource.virtualenv do
+                    path new_resource.virtualenv
+                    interpreter new_resource.python_interpreter
+                    action :create
+                end
+            end
+
+            # Workaround for http://tickets.opscode.com/browse/CHEF-2784 -
+            # Chef sets LC_ALL=C on execute, which causes pip to fail if
+            # there's anything encoded in the setup.py. Like above the entire
+            # before_migrate action isn't copied since I don't need all the
+            # flexibility.
+            def action_before_migrate
+                if new_resource.requirements
+                  Chef::Log.info("Installing using requirements file: #{new_resource.requirements}")
+                  pip_cmd = ::File.join(new_resource.virtualenv, 'bin', 'pip')
+                  execute "#{pip_cmd} install --source=#{Dir.tmpdir} -r #{new_resource.requirements}" do
+                    cwd new_resource.release_path
+                    environment "LC_ALL" => ENV['LANG']
+                  end
+                else
+                  Chef::Log.debug("No requirements file found")
+                end
+            end
+
+        end
+    end
 end
 
 #
@@ -52,8 +68,7 @@ end
 end
 
 # pip really doesn't like being run without a good env encoding, so fix that.
-ENV["LANG"] = "en_US.UTF-8"
-ENV["LC_CTYPE"] = "en_US.UTF-8"
+ENV['LANG'] = "en_US.UTF-8"
 
 # It's a secret to everyone.
 secrets = data_bag_item("secrets", "pydotorg-redesign")
