0e040f6b41be85cd80fb969bed9388659e39ffa3
Noah Kantrowitz <noah@coderanger.net>
2013-03-05 10:37:14 -0800

    Add pydotorg preview/staging server.

--- site-cookbooks/psf-loadbalancer/recipes/haproxy.rb
+++ site-cookbooks/psf-loadbalancer/recipes/haproxy.rb
@@ -10,5 +10,6 @@ haproxy_section 'python' do
     :pypi_servers => search(:node, 'roles:pypi AND tags:active'),
     :wiki_servers => search(:node, 'roles:wiki AND tags:active'),
     :pypy_home_servers => search(:node, 'roles:pypy-home AND tags:active'),
+    :preview_servers => search(:node, 'roles:redesign-staging'),
   })
 end
