645426238e6b8031d680b7d8ad2d55eb468d9dc2
Benjamin W. Smith <benjaminwarfield@just-another.net>
2013-03-19 13:01:10 -0400

    Template the riemann config.

--- site-cookbooks/monitoring/recipes/default.rb
+++ site-cookbooks/monitoring/recipes/default.rb
@@ -1,3 +1,11 @@
 include_recipe 'runit'
 include_recipe 'riemann::server'
 include_recipe 'graphite'
+
+template '/etc/riemann/riemann.config' do
+  source 'riemann.config.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
+  notifies :restart, resources(:service => 'riemann')
+end
