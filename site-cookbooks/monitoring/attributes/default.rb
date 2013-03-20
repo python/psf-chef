ec3f7470f73fa9ab54d58e1730c17a666b54ddcc
Benjamin W. Smith <benjaminwarfield@just-another.net>
2013-03-20 17:49:25 -0400

    Get firewall rules in place

--- site-cookbooks/monitoring/attributes/default.rb
+++ site-cookbooks/monitoring/attributes/default.rb
@@ -1,3 +1,6 @@
 node.set['graphite']['carbon']['line_receiver_port'] = 2002
-node.set['riemann']['server'] = '127.0.0.1'
+node.set['graphite']['carbon']['line_receiver_interface'] = "127.0.0.1"
+node.set['graphite']['carbon']['pickle_receiver_interface'] = "127.0.0.1"
+node.set['graphite']['carbon']['cache_query_interface'] = "127.0.0.1"
+node.set['riemann']['server'] = '140.211.10.83'
 node.set['collectd']['version'] = '4.10'
--- site-cookbooks/monitoring/recipes/server.rb
+++ site-cookbooks/monitoring/recipes/server.rb
@@ -2,6 +2,7 @@ include_recipe 'runit'
 include_recipe 'riemann::server'
 include_recipe 'graphite'
 include_recipe 'monitoring::client'
+include_recipe 'firewall'
 
 %w{ruby1.9.3 rubygems}.each do |pkg|
   package pkg do
@@ -20,4 +21,27 @@ template '/etc/riemann/riemann.config' do
   group 'root'
   mode 0644
   notifies :restart, resources(:service => 'riemann')
+end
+
+firewall "ufw" do
+  action :enable
+end
+
+firewall_rule "ssh" do
+  port 22
+  protocol :tcp
+  action :allow
+end
+
+firewall_rule "http" do
+  port 80
+  protocol :tcp
+  action :allow
+end
+
+firewall_rule "riemann_our_net" do
+  port 5555
+  source "140.211.10.64/26"
+  direction :in
+  action :allow
 end
