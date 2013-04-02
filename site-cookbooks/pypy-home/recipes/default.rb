0b0d4ad35da4ebaa9978d14b5a0242adaf9ac652
Noah Kantrowitz <noah@coderanger.net>
2013-04-02 10:51:28 -0700

    Hg rage.

--- site-cookbooks/pypy-home/recipes/default.rb
+++ site-cookbooks/pypy-home/recipes/default.rb
@@ -1,5 +1,23 @@
 include_recipe 'mercurial'
 
+# Temporary workaround because the hg provider computes the target rev
+# locally before pulling, so of course it is always the old. The right fix is
+# to either always pull (lame) or use the hg API to enumerate hashes on the
+# remote server. See http://stackoverflow.com/a/11900786/78722 for the latter.
+if ::File.exists?('/srv/pypy.org/shared/cached-copy/.hg')
+  execute 'hg pull' do
+    user 'root'
+    group 'root'
+    cwd '/srv/pypy.org/shared/cached-copy'
+  end
+
+  execute 'hg checkout -C extradoc' do
+    user 'root'
+    group 'root'
+    cwd '/srv/pypy.org/shared/cached-copy'
+  end
+end
+
 application "pypy.org" do
   path "/srv/pypy.org"
   repository "https://bitbucket.org/pypy/pypy.org"
