93220bbd86316361ba5601d93054571bfd6c1fbb
Benjamin W. Smith <benjaminwarfield@just-another.net>
2013-05-29 01:34:22 -0400

    remove cookbook from gitignore. move everything into cookbook dir. berks init everything.

---
+++ cookbooks/stud/metadata.rb
@@ -0,0 +1,6 @@
+name              "stud"
+maintainer        "Benjamin W. Smith"
+maintainer_email  "benjaminwarfield@just-another.net"
+license           "Apache 2.0"
+description       "Install and configure stud"
+version           "0.0.2"
