067e211c43a8d5a304b9beaf05aec218df4331bb
Donald Stufft <donald@stufft.io>
2013-06-04 10:49:41 -0400

    Forward Fastly Log messages to PyPI Syslog servers

--- roles/pypi.rb
+++ roles/pypi.rb
@@ -1,3 +1,13 @@
 name "pypi"
 description "Python package index server"
-run_list 'recipe[psf-pypi::pgbouncer]'
+run_list [
+    'recipe[psf-pypi::pgbouncer]',
+    'recipe[rsyslog::server]',
+]
+override_attributes({
+  :rsyslog => {
+    :port => 51450,
+    :per_host_dir => "%HOSTNAME%",
+    :high_precision_timestamps => true,
+  },
+})
