49eadf96eec9d69a88099a05c4bc2269c514dc4a
Donald Stufft <donald@stufft.io>
2013-06-04 23:16:25 -0400

    Move logging so that CDN log files end up in date based locations

--- cookbooks/psf-pypi/attributes/default.rb
+++ cookbooks/psf-pypi/attributes/default.rb
@@ -1,2 +1,2 @@
 default["pypi"]["cdn"]["logging"]["app_name"] = "pypi-cdn"
-default["pypi"]["cdn"]["logging"]["log_dir"] = "/var/log/pypi"
+default["pypi"]["cdn"]["logging"]["log_filename"] = "/var/log/pypi/cdn/%$YEAR%%$MONTH%%$DAY%.log"
