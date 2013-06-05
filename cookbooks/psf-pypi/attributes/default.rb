5cbda683eb66226e0cc164a69838a3ddddbeedfc
Donald Stufft <donald@stufft.io>
2013-06-05 19:32:33 -0400

    Don't use date based logging

--- cookbooks/psf-pypi/attributes/default.rb
+++ cookbooks/psf-pypi/attributes/default.rb
@@ -1,2 +1,2 @@
 default["pypi"]["cdn"]["logging"]["app_name"] = "pypi-cdn"
-default["pypi"]["cdn"]["logging"]["log_filename"] = "/var/log/pypi/cdn/%$YEAR%%$MONTH%%$DAY%.log"
+default["pypi"]["cdn"]["logging"]["log_filename"] = "/var/log/pypi/cdn/access.log"
