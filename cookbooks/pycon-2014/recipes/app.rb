bafb7edce53b61dd820599ec8c649733c2bae211
Dan Poirier <dpoirier@caktusgroup.com>
2013-06-17 14:09:52 -0400

    Updates to Pycon 2014 staging
    Switch from generic symposion to the Pycon 2014 site under development.

--- cookbooks/pycon-2014/recipes/app.rb
+++ cookbooks/pycon-2014/recipes/app.rb
@@ -2,15 +2,15 @@ db = data_bag_item("secrets", "postgres")["pycon2014"]
 
 application "staging-pycon.python.org" do
   path "/srv/staging-pycon.python.org"
-  repository "git://github.com/pinax/symposion.git"
+  repository "git://github.com/caktus/pycon.git"
   packages ["libpq-dev", "git-core", "mercurial", "subversion"]
   migrate true
 
   django do
-    requirements "requirements.txt"
+    requirements "requirements/project.txt"
     packages ["psycopg2", "gunicorn"]
     settings_template "local_settings.py.erb"
-    local_settings_file "symposion_project/local_settings.py"
+    local_settings_file "local_settings.py"
     collectstatic "collectstatic --noinput"
     database do
       engine "postgresql_psycopg2"
@@ -28,7 +28,7 @@ application "staging-pycon.python.org" do
   nginx_load_balancer do
     application_server_role "pycon-2014"
     server_name [node['fqdn'], 'staging-pycon.python.org']
-    static_files "/site_media/static" => "symposion_project/site_media/static"
+    static_files "/site_media/static" => "site_media/static"
     application_port 8080
   end
 end
