f26f465fc068323f879f8b93041d6f20f6d88cea
Benjamin W. Smith <benjaminwarfield@just-another.net>
2013-07-18 09:54:24 -0400

    Bump

--- cookbooks/psf-monitoring/metadata.rb
+++ cookbooks/psf-monitoring/metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Benjamin W. Smith"
 maintainer_email  "benjaminwarfield@just-another.net"
 license           "Apache 2.0"
 description       "Install and configure Monitoring Tools"
-version           "0.0.3"
+version           "0.0.4"
 
 depends "riemann"
 depends "runit"
