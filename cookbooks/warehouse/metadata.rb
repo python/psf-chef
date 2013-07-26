9cc5c1c94aa1b3284e68d83cd2174ad0f032749e
Donald Stufft <donald@stufft.io>
2013-07-26 02:18:52 -0400

    Add an empty Warehouse cookbook

---
+++ cookbooks/warehouse/metadata.rb
@@ -0,0 +1,6 @@
+name              "warehouse"
+maintainer        "Donald Stufft"
+maintainer_email  "donald@stufft.io"
+license           "Apache 2.0"
+description       "Install and configure Warehouse"
+version           "0.0.1"
