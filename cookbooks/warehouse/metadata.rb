72a2d2a44509f99b8eb4691af12a2d81aae936f5
Donald Stufft <donald@stufft.io>
2013-07-27 14:58:53 -0400

    Deploy Warehouse using a Chef cookbook

--- cookbooks/warehouse/metadata.rb
+++ cookbooks/warehouse/metadata.rb
@@ -4,3 +4,8 @@ maintainer_email  "donald@stufft.io"
 license           "Apache 2.0"
 description       "Install and configure Warehouse"
 version           "0.0.1"
+
+depends "git"
+depends "postgresql"
+depends "application_python"
+depends "application_nginx"
