32178e6c6263dafd1b21c1b98e46d4e3ff67aeea
Ernest W. Durbin III <ewdurbin@gmail.com>
2013-07-29 09:48:03 -0400

    firewall the pycon servers to disallow direct access

--- cookbooks/pycon-2014/metadata.rb
+++ cookbooks/pycon-2014/metadata.rb
@@ -3,10 +3,11 @@ maintainer_email "ewdurbin@gmail.com"
 license          "Apache 2.0"
 description      "Configuration for pycon 2014 staging"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.19"
+version          "0.0.20"
 
 depends "sudo"
 depends "application_python"
 depends "application_nginx"
 depends "nodejs"
 depends "git"
+depends "firewall"
