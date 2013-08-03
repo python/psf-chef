3e8ef0fdb114b9cf957a5ae37b08aa812be17156
Noah Kantrowitz <noah@coderanger.net>
2013-08-02 18:34:02 -0700

    Don't try and back up the chroots on the coverity box.

--- cookbooks/rsnapshot/recipes/backupall.rb
+++ cookbooks/rsnapshot/recipes/backupall.rb
@@ -6,6 +6,7 @@ rsnapshot_backup '/' do
   exclude '/sys/*'
   exclude '/tmp/*'
   exclude '/var/cache/apt/archives/*'
+  exlucde '/var/lib/schroot/*'
   exclude '/var/lock/*'
   exclude '/var/run/*'
   exclude '/var/tmp/*'
