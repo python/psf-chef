0d12d9d789bbc8058d2f33849a165b9d418c48e5
Ernest W. Durbin III <ewdurbin@gmail.com>
2013-08-17 09:15:20 -0400

    bump version for deploy

--- cookbooks/pycon-2014/metadata.rb
+++ cookbooks/pycon-2014/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "ewdurbin@gmail.com"
 license          "Apache 2.0"
 description      "Configuration for pycon 2014 staging"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.21"
+version          "0.0.22"
 
 depends "sudo"
 depends "application_python"
