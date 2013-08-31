46fbd2d2f48ee25d87f0b9531288ef2752c201d1
Noah Kantrowitz <noah@coderanger.net>
2013-08-30 19:55:54 -0700

    Add g.pypi.python.org to LB.

--- cookbooks/psf-loadbalancer/metadata.rb
+++ cookbooks/psf-loadbalancer/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "noah@coderanger.net"
 license          "Apache 2.0"
 description      "Configuration related to the PSF load balancers"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.7"
+version          "0.0.8"
 
 depends "heartbeat"
 #depends "jn_sysctl"
