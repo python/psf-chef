5230e75e8b9e44cfb5c670bad0ca62f0ef5e49f9
Noah Kantrowitz <noah@coderanger.net>
2013-09-06 17:03:08 -0700

    Make sure apport is removed from hg machines care of __ap__.

---
+++ cookbooks/psf-mercurial/recipes/default.rb
@@ -0,0 +1,5 @@
+# Apport conflicts with mercurial's demandimport, reported by __ap__
+
+package 'apport' do
+  action :remove
+end
--- roles/hg.rb
+++ roles/hg.rb
@@ -1,3 +1,4 @@
 name "hg"
 description "Python mercurial server"
 # Owners: Antoine Pitrou and Georg Brandl
+run_list ['recipe[psf-mercurial]']
