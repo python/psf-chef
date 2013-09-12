c954eeba692564f87eb38b20555e7681edfe8bab
Donald Stufft <donald@stufft.io>
2013-09-11 22:53:46 -0400

    Don't make the database variable pypi specific

--- cookbooks/psf-pypi/recipes/pgbouncer.rb
+++ cookbooks/psf-pypi/recipes/pgbouncer.rb
@@ -1,17 +1,17 @@
 include_recipe 'pgbouncer'
 
-database = data_bag_item('secrets', 'postgres')['pypi']
+database = data_bag_item('secrets', 'postgres')
 
 directory '/var/run/postgresql' do
   owner 'pgbouncer'
 end
 
-pgbouncer_database database['database'] do
-  host database['hostname']
-  user database['user']
-  password database['password']
+pgbouncer_database database['pypi']['database'] do
+  host database['pypi']['hostname']
+  user database['pypi']['user']
+  password database['pypi']['password']
 end
 
-pgbouncer_user database['user'] do
-  password database['password']
+pgbouncer_user database['pypi']['user'] do
+  password database['pypi']['password']
 end
