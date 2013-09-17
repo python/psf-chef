44564ee1ed112168858e38bf49b7b2df4396f72f
Donald Stufft <donald@stufft.io>
2013-09-16 21:15:17 -0400

    bump version

--- cookbooks/warehouse/metadata.rb
+++ cookbooks/warehouse/metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Donald Stufft"
 maintainer_email  "donald@stufft.io"
 license           "Apache 2.0"
 description       "Install and configure Warehouse"
-version           "0.0.1"
+version           "0.0.2"
 
 depends "apt"
 depends "python"
