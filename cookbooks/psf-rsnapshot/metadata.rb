008ca95661116fc528001d886953e175d7d492bd
Noah Kantrowitz <noah@coderanger.net>
2013-09-16 21:22:58 -0700

    Bump rev.

--- cookbooks/psf-rsnapshot/metadata.rb
+++ cookbooks/psf-rsnapshot/metadata.rb
@@ -3,6 +3,6 @@ maintainer        "Noah Kantrowitz"
 maintainer_email  "noah@coderanger.net"
 license           "Apache 2.0"
 description       "Setup backups for all PSF specific things"
-version           "0.0.1"
+version           "0.0.2"
 
 depends           "rsnapshot"
