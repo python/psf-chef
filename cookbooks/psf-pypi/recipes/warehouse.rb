2b6fe5244efb97d543a0247be6546291efad24e6
Donald Stufft <donald@stufft.io>
2013-09-17 00:05:59 -0400

    Deploy a testpypi side of Warehouse

--- cookbooks/psf-pypi/recipes/warehouse.rb
+++ cookbooks/psf-pypi/recipes/warehouse.rb
@@ -1,5 +1,5 @@
 # Get our Database settings
-database = data_bag_item("secrets", "postgres")["pypi"]
+database = data_bag_item("secrets", "postgres")
 
 # Get our secrets
 secrets = data_bag_item("secrets", "pypi")
@@ -26,10 +26,36 @@ warehouse "pypi" do
   domains node["warehouse"]["domains"]
   secret_key secrets["warehouse"]["secret_key"]
   database ({
-    :database => database["database"],
+    :database => database["pypi"]["database"],
     :host => "localhost",
-    :username => database["user"],
-    :password => database["password"],
+    :username => database["pypi"]["user"],
+    :password => database["pypi"]["password"],
+    :engine => "postgresql_psycopg2",
+  })
+end
+
+warehouse "testpypi" do
+  path "/srv/testwarehouse"
+
+  packages ({
+    "raven" => :latest,
+  })
+
+  installed_apps [
+    "raven.contrib.django.raven_compat",
+  ]
+
+  environment ({
+    "SENTRY_DSN" => secrets["sentry"]["dsn"],
+  })
+
+  domains ["test.preview.pypi.python.org"]
+  secret_key secrets["warehouse"]["secret_key"]
+  database ({
+    :database => database["testpypi"]["database"],
+    :host => "localhost",
+    :username => database["testpypi"]["user"],
+    :password => database["testpypi"]["password"],
     :engine => "postgresql_psycopg2",
   })
 end
