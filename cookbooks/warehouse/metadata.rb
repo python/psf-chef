9b992c8915e2f2a1ee113e366967f821d6b02fe0
Donald Stufft <donald@stufft.io>
2013-10-02 22:08:02 -0400

    Redirect SSL

--- cookbooks/warehouse/metadata.rb
+++ cookbooks/warehouse/metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Donald Stufft"
 maintainer_email  "donald@stufft.io"
 license           "Apache 2.0"
 description       "Install and configure Warehouse"
-version           "0.0.8"
+version           "0.0.9"
 
 depends "apt"
 depends "python"
