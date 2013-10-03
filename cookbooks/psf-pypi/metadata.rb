d0b3d9d78c3d116ebae8817652701c4923dab8bf
Donald Stufft <donald@stufft.io>
2013-10-02 21:04:48 -0400

    bump versions

--- cookbooks/psf-pypi/metadata.rb
+++ cookbooks/psf-pypi/metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Noah Kantrowitz"
 maintainer_email  "noah@coderanger.net"
 license           "Apache 2.0"
 description       "Installs and configures PyPI"
-version           "0.0.12"
+version           "0.0.13"
 
 depends           "pgbouncer"
 depends           "rsyslog"
