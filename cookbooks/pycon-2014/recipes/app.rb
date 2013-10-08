f10900d958be72b65891ac478bee3e50405aed96
Noah Kantrowitz <noah@coderanger.net>
2013-10-08 11:00:55 -0700

    Bump the time to 0020 so both jobs don't land at the same time, and add user.

--- cookbooks/pycon-2014/recipes/app.rb
+++ cookbooks/pycon-2014/recipes/app.rb
@@ -107,12 +107,14 @@ cron "staging-pycon account expunge" do
   hour "0"
   minute "0"
   command "cd /srv/staging-pycon.python.org/current && /srv/staging-pycon.python.org/shared/env/bin/python manage.py expunge_deleted"
+  user "www-data"
 end
 
 cron "staging-pycon update tutorial registrants" do
   hour "0"
-  minute "0"
+  minute "20"
   command "cd /srv/staging-pycon.python.org/current && /srv/staging-pycon.python.org/shared/env/bin/python manage.py update_tutorial_registrants"
+  user "www-data"
 end
 
 firewall 'ufw' do
