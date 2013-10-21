6a8cb65fed3ab91b9a5318ed2a58b73dd3975581
Donald Stufft <donald@stufft.io>
2013-10-20 23:33:23 -0400

    Updates to the Warehouse cookbook for a new Warehouse

--- cookbooks/psf-pypi/metadata.rb
+++ cookbooks/psf-pypi/metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Noah Kantrowitz"
 maintainer_email  "noah@coderanger.net"
 license           "Apache 2.0"
 description       "Installs and configures PyPI"
-version           "0.0.13"
+version           "0.0.14"
 
 depends           "pgbouncer"
 depends           "rsyslog"
