7400ed85281fb39cb1843a2a864ba487e4b521e7
Donald Stufft <donald@stufft.io>
2013-11-07 20:28:42 -0500

    Add dh-virtualenv to our built in reprepro

--- cookbooks/psf-debbuild/recipes/default.rb
+++ cookbooks/psf-debbuild/recipes/default.rb
@@ -5,3 +5,11 @@ cookbook_file "python-virtualenv_1.10.1-1_all.deb" do
 end
 
 reprepro_deb "/tmp/python-virtualenv_1.10.1-1_all.deb"
+
+# Put our dh-virtualenv package into reprepro
+cookbook_file "dh-virtualenv_0.6_all.deb" do
+  path "/tmp/dh-virtualenv_0.6_all.deb"
+  action :create_if_missing
+end
+
+reprepro_deb "/tmp/dh-virtualenv_0.6_all.deb"
