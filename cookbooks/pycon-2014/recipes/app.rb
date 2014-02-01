1d1d68263c609f3027b18350377f0f02c9b526f3
Noah Kantrowitz <noah@coderanger.net>
2014-01-31 23:06:45 -0800

    Try making the cron job work correctly.

--- cookbooks/pycon-2014/recipes/app.rb
+++ cookbooks/pycon-2014/recipes/app.rb
@@ -106,13 +106,13 @@ end
 cron_d "staging-pycon-account-expunge" do
   hour "0"
   minute "0"
-  command "source /srv/staging-pycon.python.org/shared/.env && cd /srv/staging-pycon.python.org/current && /srv/staging-pycon.python.org/shared/env/bin/python manage.py expunge_deleted"
+  command "bash -c 'source /srv/staging-pycon.python.org/shared/.env && cd /srv/staging-pycon.python.org/current && /srv/staging-pycon.python.org/shared/env/bin/python manage.py expunge_deleted'"
 end
 
 cron_d "staging-pycon-update-tutorial-registrants" do
   hour "0"
   minute "20"
-  command "source /srv/staging-pycon.python.org/shared/.env && cd /srv/staging-pycon.python.org/current && /srv/staging-pycon.python.org/shared/env/bin/python manage.py update_tutorial_registrants"
+  command "bash -c 'source /srv/staging-pycon.python.org/shared/.env && cd /srv/staging-pycon.python.org/current && /srv/staging-pycon.python.org/shared/env/bin/python manage.py update_tutorial_registrants'"
 end
 
 firewall 'ufw' do
