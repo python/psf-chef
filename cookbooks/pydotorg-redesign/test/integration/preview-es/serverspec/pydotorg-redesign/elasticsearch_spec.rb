448607343313a55189d0fda97796b97189de3f25
Benjamin W. Smith <benjaminwarfield@just-another.net>
2014-02-14 13:18:17 -0500

    Sneak in another silly test.

--- cookbooks/pydotorg-redesign/test/integration/preview-es/serverspec/pydotorg-redesign/elasticsearch_spec.rb
+++ cookbooks/pydotorg-redesign/test/integration/preview-es/serverspec/pydotorg-redesign/elasticsearch_spec.rb
@@ -8,5 +8,9 @@ describe 'pydotorg-redesign::elasticsearch' do
   it 'will run be running ok' do
     command("curl -s -XGET 'http://localhost:9200/_status' | grep ok").should return_exit_status 0
   end 
+
+  it 'will be running with 256m max' do
+    command('ps aux | grep elas | grep mx256m').should return_exit_status 0
+  end
 end
 
