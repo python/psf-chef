a7db37619891e86d0bf0cdb8a66f889847083d94
Benjamin W. Smith <benjaminwarfield@just-another.net>
2014-02-19 20:32:01 -0500

    Dont need --noinput on these

--- cookbooks/pydotorg-redesign/recipes/crons.rb
+++ cookbooks/pydotorg-redesign/recipes/crons.rb
@@ -1,13 +1,13 @@
 current_env = node['pydotorg-redesign']['env']
 
 cron_d 'import-blog-feeds' do
-  command "/srv/redesign.python.org/shared/env/bin/python /srv/redesign.python.org/current/manage.py update_blogs --settings pydotorg.settings.#{current_env} --noinput"
+  command "/srv/redesign.python.org/shared/env/bin/python /srv/redesign.python.org/current/manage.py update_blogs --settings pydotorg.settings.#{current_env}"
   user 'www-data'
   hour 0
 end
 
 cron_d 'import-ics-events' do
-  command "/srv/redesign.python.org/shared/env/bin/python /srv/redesign.python.org/current/manage.py import_ics_calendars --settings pydotorg.settings.#{current_env} --noinput"
+  command "/srv/redesign.python.org/shared/env/bin/python /srv/redesign.python.org/current/manage.py import_ics_calendars --settings pydotorg.settings.#{current_env}"
   user 'www-data'
   minute 0
   hour 3
