7ffc238fc1f1782aadf6afa9131c568acc141485
Ernest W. Durbin III <ewdurbin@gmail.com>
2014-05-20 17:27:18 -0400

    metadata bumpin'

--- cookbooks/pycon-2014/metadata.rb
+++ cookbooks/pycon-2014/metadata.rb
@@ -1,9 +1,9 @@
 maintainer       "Ernest W. Durbin III"
 maintainer_email "ewdurbin@gmail.com"
 license          "Apache 2.0"
-description      "Configuration for pycon 2014 staging and production"
+description      "Configuration for us.pycon.org staging and production"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.34"
+version          "0.0.35"
 
 depends "sudo"
 depends "application_python"
