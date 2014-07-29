a90b4c8ae091d9abbf8e61d21655f914dd4dff9a
Benjamin W. Smith <benjaminwarfield@just-another.net>
2014-07-29 16:54:48 -0400

    bump psf-pycon

--- cookbooks/psf-pycon/metadata.rb
+++ cookbooks/psf-pycon/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "ewdurbin@gmail.com"
 license          "Apache 2.0"
 description      "Configuration for us.pycon.org staging and production"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.36"
+version          "0.0.37"
 
 depends "sudo"
 depends "application_python"
