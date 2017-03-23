e1bb9d35bf0fe1a6563f074503cafedb6bd43a67
Ernest W. Durbin III <ewdurbin@gmail.com>
2017-03-23 14:38:18 -0400

    bump pypy-home cookbook version

--- cookbooks/pypy-home/metadata.rb
+++ cookbooks/pypy-home/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "alex.gaynor@gmail.com"
 license          "Apache 2.0"
 description      "Configuration for pypy.org"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 
 depends "sudo"
 depends "application_nginx"
